746c0c878aae3ae836b50641ac0a70c4bee72726
naimatofficial <naimat.official.10@gmail.com>
2025-01-08 12:43:19 +0500

    update category

--- src/components/Categories/CategoryItem.jsx
+++ src/components/Categories/CategoryItem.jsx
@@ -12,11 +12,11 @@ const CategoryItem = ({ category }) => {
     return (
         <Link
             to={`/products/category/${category.slug}`}
-            className="flex-center flex-col gap-2 p-2 group cursor-pointer mt-6"
+            className="flex flex-col justify-center items-center gap-2 p-2 group cursor-pointer mt-6"
         >
             <div
                 className="relative w-28 h-28 flex items-center justify-center overflow-hidden 
-            rounded-full border border-gray-100 transition-all duration-300 
+            rounded-full border border-gray-100  transition-all duration-300 
             ease-out group-hover:shadow-lg"
             >
                 <img
@@ -25,7 +25,7 @@ const CategoryItem = ({ category }) => {
                     className="w-full h-full object-cover transition-transform duration-300 ease-out group-hover:scale-110"
                 />
             </div>
-            <p className="text-sm w-28 truncate transition-colors duration-300 ease-out text-gray-800 group-hover:text-primary-500">
+            <p className="text-sm text-center w-28 truncate transition-colors duration-300 ease-out text-gray-800 group-hover:text-primary-500">
                 {category.name}
             </p>
         </Link>
